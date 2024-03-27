c57b171e094193db3819b01f697a6d15a382b89d
Alan Chou <alan.chou@me.com>
2024-03-27 16:05:22 +0800

    Initial Commit

---
+++ MyBooksSample/MyBooksSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksSampleApp.swift
+//  MyBooksSample
+//
+//  Created by Alan Chou on 2024/3/27.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
